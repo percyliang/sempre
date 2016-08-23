d67a6302329a19f4cf2f1cc057df5c364f949be9
Sida Wang <sidaw@cs.stanford.edu>
2016-08-23 16:20:56 -0700

    better features

--- src/edu/stanford/nlp/sempre/interactive/actions/BeamFloatingFeatureComputer.java
+++ src/edu/stanford/nlp/sempre/interactive/actions/BeamFloatingFeatureComputer.java
@@ -17,13 +17,13 @@ public class BeamFloatingFeatureComputer implements FeatureComputer {
   public static class Options {
     @Option(gloss = "Verbosity")
     public int verbose = 0;
-    
+
     @Option(gloss = "the N in N-gram")
     public int ngramN = 3;
 
     @Option(gloss = "which categories to parameterize")
     public List<String> parameterizeCats = Lists.newArrayList("$Number", "$Dates");
-    
+
     @Option(gloss = "prefixing indicating that the token corresponds to current derivation")
     public String anchorString = "!!";
   }
@@ -61,9 +61,12 @@ public class BeamFloatingFeatureComputer implements FeatureComputer {
         func += " " + getSubtreeFeature(actionFormula.args.get(i), depth - 1, anchored);
       }
       return func + ")";
+    } else if (f instanceof JoinFormula) {
+      JoinFormula joinFormula = (JoinFormula) f;
+      return "(" + getSubtreeFeature(joinFormula.relation, depth-1, anchored)+ "," +  getSubtreeFeature(joinFormula.child, depth-1, anchored)+")";
     } else if (f instanceof LambdaFormula) {
       LambdaFormula lambdaFormula = (LambdaFormula) f;
-      return "lambda_"+ lambdaVar(lambdaFormula) + "." + getSubtreeFeature(lambdaFormula.body, depth - 1, anchored);
+      return "lambda "+ lambdaVar(lambdaFormula) + "." + getSubtreeFeature(lambdaFormula.body, depth - 1, anchored);
     } else if (f instanceof ValueFormula<?>) {
       ValueFormula<?> valueFormula = (ValueFormula<?>) f;
       if (valueFormula.value instanceof NumberValue) {
@@ -74,23 +77,41 @@ public class BeamFloatingFeatureComputer implements FeatureComputer {
     } else if (f instanceof VariableFormula) {
       VariableFormula variableFormula = (VariableFormula) f;
       return variableFormula.name;
-    } else throw new RuntimeException("Unhandled formula type: " + f.getClass().toString());
+    } else if (f instanceof ReverseFormula) {
+      ReverseFormula reverseFormula = (ReverseFormula) f;
+      return getSubtreeFeature(reverseFormula.child, depth, anchored);
+    } else if (f instanceof MergeFormula) {
+      MergeFormula mergeFormula = (MergeFormula) f;
+      return "(" + mergeFormula.mode + " " + getSubtreeFeature(mergeFormula.child1, depth-1, anchored) + "," + getSubtreeFeature(mergeFormula.child2, depth-1, anchored) + ")";
+    } else {
+      throw new RuntimeException("Unhandled formula type: " + f.getClass().toString());
+    }
   }
 
   // function to abstract out ALL anchored stuff in the utterance.
   private List<String> parametrizeAnchors(List<String> tokens, Derivation deriv) {
     if (deriv.start == -1) 
       return tokens;
-    
     List<String> newTokens = new ArrayList<>();
     newTokens.addAll(tokens.subList(0, deriv.start));
     for (int i = deriv.start; i < deriv.end; i++) {
-        newTokens.add(opts.anchorString);
+      newTokens.add(opts.anchorString);
     }
     newTokens.addAll(tokens.subList(deriv.end, tokens.size()));
     return newTokens;
   }
   
+  private void processUtterance(Derivation deriv, List<String> tokens) {
+    if (deriv == null || deriv.start == -1) 
+      return;
+    if (deriv.rule.isAnchored()) {
+      for (int i = deriv.start; i<deriv.end; i++)
+        tokens.set(i, opts.anchorString);
+    }
+    for (Derivation child : deriv.children)
+      processUtterance(child, tokens);
+  }
+
   private List<String> getAllNgrams(List<String> tokens, int n, Derivation deriv) {
     List<String> ngrams = new ArrayList<>();
     List<String> paddedTokens = new ArrayList<>();
@@ -124,34 +145,39 @@ public class BeamFloatingFeatureComputer implements FeatureComputer {
     return ngrams;
   }
 
-  private void addSubtreeFeatures(Example ex, Derivation deriv) {
-    if (!FeatureExtractor.containsDomain("subtree")) return;
-    if (deriv.rule != Rule.nullRule) {
-      List<String> anchorAbstractTokens = parametrizeAnchors(ex.languageInfo.tokens, deriv);
-      //List<String> anchorAbstractTokens = ex.languageInfo.tokens;
-      if (deriv.start!=-1) {
-        for (int n=1; n<=opts.ngramN; n++) {
-          for (String ngram : getAllNgrams(anchorAbstractTokens, n, deriv)) {
-            deriv.addFeature("anchored", getSubtreeFeature(deriv.formula, 3, true) + " :: " + ngram);
-            deriv.addFeature("anchored", getSubtreeFeature(deriv.formula, 2, true) + " :: " + ngram);
-            deriv.addFeature("anchored", getSubtreeFeature(deriv.formula, 1, true) + " :: " + ngram);
-          }
-          for (String ngram : getAllSkipGrams(anchorAbstractTokens, deriv)) {
-            deriv.addFeature("anchored", getSubtreeFeature(deriv.formula, 2, true) + " :: " + ngram);
-          }
-        }
+  private List<String> breakUtterance(List<String> currentTokens, Derivation deriv) {
+    
+  
+    // List<String> span = currentTokens.subList(deriv.start, deriv.end);
+    List<String> span = currentTokens;
+    
+    List<String> retval = new ArrayList<String>();
+    String current = "";
+    for (String tok : span) {
+      if (tok.equals(opts.anchorString) && current.length()>0) {
+        retval.add(current);
+        current = "";
       } else {
-        for (int n=1; n<=opts.ngramN; n++) {
-          for (String ngram : getAllNgrams(anchorAbstractTokens, n, deriv)) {
-            deriv.addFeature("subtree", getSubtreeFeature(deriv.formula, 1) + " :: " + ngram);
-            deriv.addFeature("subtree", getSubtreeFeature(deriv.formula, 2) + " :: " + ngram);
-            deriv.addFeature("subtree", getSubtreeFeature(deriv.formula, 3) + " :: " + ngram);
-          }
-        }
-        for (String ngram : getAllSkipGrams(anchorAbstractTokens, deriv)) {
-          deriv.addFeature("subtree", getSubtreeFeature(deriv.formula, 1) + " :: " + ngram);
-          deriv.addFeature("subtree", getSubtreeFeature(deriv.formula, 2) + " :: " + ngram);
-          deriv.addFeature("subtree", getSubtreeFeature(deriv.formula, 3) + " :: " + ngram);
+        current += " " + tok;
+      }
+    }
+    
+    if (current.length()>0) {
+      retval.add(current);
+    }
+    return retval;
+  }
+  private void addSubtreeFeatures(Example ex, Derivation deriv) {
+    if (!FeatureExtractor.containsDomain("bff")) return;
+    if (deriv.rule != Rule.nullRule) {
+      if (!deriv.allAnchored()) {
+        List<String> anchorAbstractTokens = Lists.newArrayList(ex.languageInfo.tokens);
+        processUtterance(deriv, anchorAbstractTokens);
+        //List<String> anchorAbstractTokens = ex.languageInfo.tokens;
+        
+        for (String part : breakUtterance(anchorAbstractTokens, deriv)) {
+          deriv.addFeature("bff", getSubtreeFeature(deriv.formula, 2) + " :: " + part);
+          deriv.addFeature("bff", getSubtreeFeature(deriv.formula, 3) + " :: " + part);
         }
       }
     }
